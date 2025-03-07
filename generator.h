364542654e4cbb258c9039e7ecf6c49eae4479a5
TejaDirba <teja.tupinyte@gmail.com>
2025-03-08 01:07:11 +0200

    Create generator.h

---
+++ generator.h
@@ -0,0 +1,10 @@
+#ifndef GENERATOR_H
+#define GENERATOR_H
+
+#include "student.h"
+#include <vector>
+#include <string>
+
+void generateStudentFile(const std::string& filename, int studentCount);
+
+#endif // GENERATOR_H
