a0dd2703ee26eda8564deaf3b9b62d77d7e4c344
TejaDirba <teja.tupinyte@gmail.com>
2025-06-15 17:22:58 +0300

    Update student.h

--- student.h
+++ student.h
@@ -13,21 +13,33 @@ private:
     int egzaminas;
     double galutinisVid;
     double galutinisMed;
+
 public:
     Student();
     Student(const std::string& v, const std::string& p);
+
+    // Rule of Five
+    Student(const Student& other);
+    Student(Student&& other) noexcept;
+    Student& operator=(const Student& other);
+    Student& operator=(Student&& other) noexcept;
+    ~Student();
+
     std::string getVardas() const;
     std::string getPavarde() const;
     std::vector<int> getNd() const;
     int getEgzaminas() const;
     double getGalutinisVid() const;
     double getGalutinisMed() const;
+
     void setVardas(const std::string& v);
     void setPavarde(const std::string& p);
     void setNd(const std::vector<int>& grades);
     void setEgzaminas(int e);
     void addHomeworkGrade(int grade);
+
     void calculateFinalGrades();
+
     friend std::istream& operator>>(std::istream& is, Student& s);
     friend std::ostream& operator<<(std::ostream& os, const Student& s);
 };
@@ -38,4 +50,4 @@ bool compareByMedian(const Student& a, const Student& b);
 double calculateAverage(const std::vector<int>& v);
 double calculateMedian(std::vector<int> v);
 
-#endif
+#endif // STUDENT_H
