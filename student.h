52375871be5f301fe9d8a8a431d21df1fbff6b1e
TejaDirba <teja.tupinyte@gmail.com>
2025-06-15 17:28:49 +0300

    Update student.h

--- student.h
+++ student.h
@@ -1,14 +1,12 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
-#include <string>
+#include "Zmogus.h"
 #include <vector>
 #include <iostream>
 
-class Student {
+class Student : public Zmogus {
 private:
-    std::string vardas;
-    std::string pavarde;
     std::vector<int> nd;
     int egzaminas;
     double galutinisVid;
@@ -25,29 +23,18 @@ public:
     Student& operator=(Student&& other) noexcept;
     ~Student();
 
-    std::string getVardas() const;
-    std::string getPavarde() const;
     std::vector<int> getNd() const;
     int getEgzaminas() const;
     double getGalutinisVid() const;
     double getGalutinisMed() const;
 
-    void setVardas(const std::string& v);
-    void setPavarde(const std::string& p);
     void setNd(const std::vector<int>& grades);
     void setEgzaminas(int e);
     void addHomeworkGrade(int grade);
-
     void calculateFinalGrades();
 
     friend std::istream& operator>>(std::istream& is, Student& s);
     friend std::ostream& operator<<(std::ostream& os, const Student& s);
 };
-
-bool compareByName(const Student& a, const Student& b);
-bool compareByAvg(const Student& a, const Student& b);
-bool compareByMedian(const Student& a, const Student& b);
-double calculateAverage(const std::vector<int>& v);
-double calculateMedian(std::vector<int> v);
 
 #endif // STUDENT_H
